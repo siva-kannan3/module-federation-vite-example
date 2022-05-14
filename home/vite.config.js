0a163d89d9e407beb2744438fe4efe2e3e70a833
sivak <sivakwalker3@gmail.com>
2022-05-14 08:25:59 +0530

    build: 🔨 federation config for both host & remote

--- home/vite.config.js
+++ home/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    federation({
+      remotes: {
+        siva: "http://localhost:4173/assets/remoteEntry.js",
+      },
+    }),
+  ],
+});
--- widgets/vite.config.js
+++ widgets/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    federation({
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/Components/Button/Button.jsx",
+      },
+    }),
+  ],
+});
